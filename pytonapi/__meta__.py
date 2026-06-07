4c6864ae124d19817fe7c0c24683e942c51a0600
ness <shonness98@gmail.com>
2026-06-08 04:37:01 +0500

    Update version to 2.2.1

--- pytonapi/__meta__.py
+++ pytonapi/__meta__.py
@@ -3,6 +3,6 @@
 # This source code is licensed under the MIT License found in the
 # LICENSE file in the root directory of this source tree.
 
-__version__ = "2.2.0"
+__version__ = "2.2.1"
 __author__ = "nessshon"
 __url__ = "https://github.com/nessshon/tonapi"
